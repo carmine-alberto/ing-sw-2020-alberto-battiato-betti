a9b9233e02a0bc286cd6131e1a28701d3d255c73
Filippo Betti <buby98@hotmail.it>
2020-03-26 12:21:52 +0100

    Classes implementation and adding new tests
    GameWorkerTests

--- src/main/java/it/polimi/ingsw/model/GameWorker.java
+++ src/main/java/it/polimi/ingsw/model/GameWorker.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameWorker {
+
     Action moveStrategy;
     FieldCell position;
     Game currentGame;
@@ -14,6 +15,10 @@ public class GameWorker {
         this.currentGame = currentGame;
     }
 
+    public void setPosition(FieldCell cell) {
+        this.position = cell;
+    }
+
     public List<FieldCell> getAvailableCells() {
         List<FieldCell> freeCells = new ArrayList<>();
         //Controllo le celle adiacenti: se delta_height_up <= 1 AND !cell.hasDome() AND cell.isFree(), la aggiungo alla lista
@@ -21,15 +26,11 @@ public class GameWorker {
         return freeCells;
     }
 
-    public void move(String args) { //TODO args non è String
-        moveStrategy.run(args, this);
+    public void move(Integer x, Integer y) { //TODO args non è String
+        moveStrategy.run(currentGame.getCell(x, y), this);
     }
 
     public FieldCell getCell() {
         return this.position;
-    }
-
-    public void setCell(String args) {
-        this.position = currentGame.getCell(args);
     }
 }
