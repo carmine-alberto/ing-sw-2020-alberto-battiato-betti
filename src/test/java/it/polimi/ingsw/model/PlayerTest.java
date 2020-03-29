6a4203096c0beba5f6cd72dd7f433984c47a24e1
batjacopo <jacopo.battiato@mail.polimi.it>
2020-03-29 18:32:35 +0200

    test for player

--- src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -3,15 +3,37 @@ package it.polimi.ingsw.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    Game game = new Game();
+    Player first = new Player("Scroto");
+    Player second = new Player("Pallido");
+    Player third = new Player("Francesco");
+    List<Player> firstOpponents = new ArrayList<>();
+    List<Player> secondOpponents = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
+        game.addPlayer(first);
+        first.setCurrentGame(game);
+        game.addPlayer(second);
+        second.setCurrentGame(game);
+        game.addPlayer(third);
+        third.setCurrentGame(game);
+        firstOpponents.add(second);
+        firstOpponents.add(third);
+        secondOpponents.add(first);
+        secondOpponents.add(third);
     }
 
     @Test
     void getOpponents() {
+        assertEquals(firstOpponents, first.getOpponents());
+        assertEquals(secondOpponents, second.getOpponents());
     }
 }
